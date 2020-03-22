9685e79c7541a9b20ed0b28d35b3db4a7f2c2cf0
edwardmartinsjr <edwardmartinsjr@gmail.com>
2020-03-22 02:49:15 +0100

    add stream listener

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def get_dbh():
                                             db_port, db_name)
     return create_engine(conn, encoding='utf-8')
 
-
 def set_logger(debug):
     fh = logging.StreamHandler()
     formatter = logging.Formatter('%(levelname)s %(message)s')
@@ -47,8 +46,67 @@ def set_logger(debug):
     else:
         LOG.setLevel(logging.INFO)
 
+# Tweepy class to access Twitter API
+class Streamlistener(tweepy.StreamListener):
+	def on_connect(self):
+		self.start_time = time.time()
+		self.limit = 60
+		LOG.info('You are connected to the Twitter API')
+
+
+	def on_error(self, status):
+		if status != 200:
+			LOG.error('error found: {}'.format(status) )
+			# Returning false disconnects the stream
+			return False
+
+	# Reads in tweet data as Json and extracts the data we want.
+	def on_data(self,data):	
+		if (time.time() - self.start_time) < self.limit:
+			try:
+				raw_data = json.loads(data)
+
+				if 'text' in raw_data: 
+					username = raw_data['user']['screen_name']
+					created_at = parser.parse(raw_data['created_at'])
+					tweet = raw_data['text']
+					retweet_count = raw_data['retweet_count']
+
+					if raw_data['place'] is not None:
+						place = raw_data['place']['country']
+						LOG.info(place)
+					else:
+						place = None
+					
+
+					location = raw_data['user']['location']
+
+					
+					LOG.info('\n USERNAME: %s \n TWEET: %s \n RETWEET: %d \n PLACE: %s \n LOCATION: %s',username, tweet, retweet_count, place, location)
+					LOG.info(' Tweet colleted at: {} \n'.format(str(created_at)))
+
+			except BaseException as e:
+				LOG.error(e)
+		else:
+			return False
 
 if __name__== '__main__':
 	set_logger(True)
 	
 	LOG.info('Start Twitter Stream Listener')
+
+	# Connects to
+	# Authentification so we can access twitter
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api =tweepy.API(auth, wait_on_rate_limit=True)
+	LOG.info('OAuth authentication')
+
+	LOG.info('Listening twitter streaming')
+	# Create instance of Streamlistener
+	listener = Streamlistener(api = api)
+	stream = tweepy.Stream(auth, listener = listener)
+
+	# Choose what we want to filter by
+	track = ['all-inclusive']
+	stream.filter(track = track, languages = ['en', 'nl'], locations = GEOBOX_NETHERLANDS)
